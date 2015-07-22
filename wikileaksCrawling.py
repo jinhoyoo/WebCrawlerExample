213e9491533c26f93d026b597c6cbef909c5435d
Jinho.Yoo <jinho.yoo@nomadconnection.com>
2015-07-22 18:33:54 +0900

    Fix bugs

--- wikileaksCrawling.py
+++ wikileaksCrawling.py
@@ -15,7 +15,8 @@ def get_mail_list_from_wikileak_search_url(search_url):
         if 0 <= attribute.find("/hackingteam/emails/emailid/" ):
             email_list.append(attribute )
 
-    email_list = get_mail_list( url, max_result )
+    # Filter out redundency mail list.
+    filtered_email_list = list(set(email_list))
     return email_list
 
 
@@ -41,7 +42,7 @@ def get_mail_list( url, max_result):
     search_url = extract_search_result_url(url)
 
     f = open("email_list_temp.json", 'w')
-    f.write( " { 'email_list' : [ ")
+    f.write( " { \"email_list\" : [ ")
 
 
     email_list = []
@@ -58,7 +59,7 @@ def get_mail_list( url, max_result):
 
         # Write data temporary.
         for elem in data:
-            f.write( "'"+elem+"',\n")
+            f.write( "\""+elem+"\",\n")
             email_list.append(elem)
 
     f.write( "] }")
@@ -69,9 +70,9 @@ def get_mail_list( url, max_result):
 
     #Save it in the file.
     f = open("email_list.json", 'w')
-    f.write( " { 'email_list' : [ ")
+    f.write( " { \"email_list\" : [ ")
     for elem in filtered_email_list:
-        f.write( "'"+elem+"',\n")
+        f.write( "\""+elem+"\",\n")
     f.write( "] }")
     f.close()
 
@@ -81,19 +82,14 @@ def get_mail_list( url, max_result):
 if __name__ == "__main__":
 
     # Search URL with Korea and Devilangel
-    url = "https://wikileaks.org/hackingteam/emails?q=Korea+%7C+deviangel&mfrom=&mto=&title=&notitle=&date=&nofrom=&noto=&count=1000&sort=0#searchresult"
+    search_url = "https://wikileaks.org/hackingteam/emails?q=Korea+%7C+deviangel&mfrom=&mto=&title=&notitle=&date=&nofrom=&noto=&count=1000&sort=0#searchresult"
 
     # Estimated max result
     max_result = 2000
 
     # get mail list from given search URL result.
-    email_list = get_mail_list( url, max_result )
+    email_list = get_mail_list( search_url, max_result )
 
     print email_list
 
 
-    """
-    url_to_extract_mails = "https://wikileaks.org/hackingteam/emails?q=Korea+%7C+deviangel&relid=0&title=&notitle=&date=&mailboxid=0&mailbox=&domainid=0&domain=&minrecipient=0&maxrecipient=0&file=&mto=&mfrom=&nofrom=&noto=&offset=50"
-    data = get_mail_list_from_wikileak_search_url(url_to_extract_mails)
-    print data
-    """
